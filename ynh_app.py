b093112b594a413b466c7c3a3bfdae0fcdf8075f
Hadrien <ketsapiwiq@protonmail.com>
2021-10-27 13:32:43 +0300

    coquille mail

--- ynh_app.py
+++ ynh_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2021, Hadrien <ketsapiwiq@protonmail.org>
+# Copyright: (c) 2021, Hadrien <ketsapiwiq@protonmail.com>
 # GNU Affero General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/agpl-3.0.txt)
 
 from subprocess import Popen, PIPE
