1751e9793975741180af2af569fc2469e09bbe21
Hadrien <ketsapiwiq@protonmail.com>
2021-10-27 14:31:02 +0300

    format: apply black

--- ynh_app.py
+++ ynh_app.py
@@ -7,13 +7,14 @@ from subprocess import Popen, PIPE
 import json
 from urllib.parse import urlencode
 from ansible.module_utils.basic import AnsibleModule
+
 ANSIBLE_METADATA = {
-    'metadata_version': '0.1',
-    'status': ['preview'],
-    'supported_by': 'community'
+    "metadata_version": "0.1",
+    "status": ["preview"],
+    "supported_by": "community",
 }
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: ynh_app
 
@@ -54,9 +55,9 @@ options:
 
 author:
     - Hadrien (@ketsapiwiq)
-'''
+"""
 
-EXAMPLES = '''
+EXAMPLES = """
 # Change app domain and settings
 - name: Update the default domain
   ynh_app:
@@ -65,9 +66,9 @@ EXAMPLES = '''
     settings:
         path: '/'
     label: Wordpress
-'''
+"""
 
-RETURN = '''
+RETURN = """
 # new_settings:
 #     description: The new settings key-values
 #     type: str
@@ -99,53 +100,66 @@ state:
 #     description: Whether the domain needed to be changed
 #     type: bool
 #     returned: always
-'''
+"""
 
 
 def get_app_info(name, verbose=False):
     try:
         if verbose:
-            command = ['/usr/bin/yunohost',
-                       'app', 'info', '--output-as', 'json', '--full', name]
+            command = [
+                "/usr/bin/yunohost",
+                "app",
+                "info",
+                "--output-as",
+                "json",
+                "--full",
+                name,
+            ]
         else:
-            command = ['/usr/bin/yunohost',
-                       'app', 'info', '--output-as', 'json', name]
-        app_info_result = Popen(
-            command,
-            stdout=PIPE,
-            stderr=PIPE
-        )
+            command = ["/usr/bin/yunohost", "app", "info", "--output-as", "json", name]
+        app_info_result = Popen(command, stdout=PIPE, stderr=PIPE)
         stdout, stderr = app_info_result.communicate()
 
-        stdout = stdout.decode('UTF-8')
-        stderr = stderr.decode('UTF-8')
+        stdout = stdout.decode("UTF-8")
+        stderr = stderr.decode("UTF-8")
         if app_info_result.returncode != 0:
             if "Could not find" in stderr:
                 return False
 
-            module.fail_json(msg='YunoHost returned an error for command: ' +
-                                 str(command) + '\nExit code:' + app_info_result.returncode + '\nError: ' + str(stderr), **result)
+            module.fail_json(
+                msg="YunoHost returned an error for command: "
+                + str(command)
+                + "\nExit code:"
+                + app_info_result.returncode
+                + "\nError: "
+                + str(stderr),
+                **result
+            )
         else:
             return json.loads(stdout)
     except OSError as e:
-        module.fail_json(msg='Could not run CLI with command: ' +
-                         str(command) + '\nError: ' + str(e), **result)
+        module.fail_json(
+            msg="Could not run CLI with command: "
+            + str(command)
+            + "\nError: "
+            + str(e),
+            **result
+        )
 
 
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        id=dict(type='str', required=False),
-        name=dict(type='str', required=False),
-        label=dict(type='str', required=False),
-        settings=dict(type='dict',
-                      required=False, default=dict()),
-        domain=dict(type='str', required=False),
+        id=dict(type="str", required=False),
+        name=dict(type="str", required=False),
+        label=dict(type="str", required=False),
+        settings=dict(type="dict", required=False, default=dict()),
+        domain=dict(type="str", required=False),
         # TODO: not implemented
-        force=dict(type='bool', required=False, default=False),
+        force=dict(type="bool", required=False, default=False),
         # TODO: not implemented
         # upgraded=dict(type='bool', required=False, default=False),
-        state=dict(type='str', required=False, default='present'),
+        state=dict(type="str", required=False, default="present"),
     )
 
     # seed the result dict in the object
@@ -153,93 +167,92 @@ def run_module():
     # changed is if this module effectively modified the target
     # state will include any data that you want your module to pass back
     # for consumption, for example, in a subsequent task
-    result = dict(
-        changed=False)
+    result = dict(changed=False)
 
     # the AnsibleModule object will be our abstraction working with Ansible
     # this includes instantiation, a couple of common attr would be the
     # args/params passed to the execution, as well as if the module
     # supports check mode
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     # We parse arguments received and check for coherence
-# TODO: test and support installing a second app by playing with id and name e.g. grav__2
+    # TODO: test and support installing a second app by playing with id and name e.g. grav__2
 
-# TODO: test coherence for this
-    app_id = module.params['id'] or module.params['name']
-    app_name = module.params['name'] or module.params['id']
+    # TODO: test coherence for this
+    app_id = module.params["id"] or module.params["name"]
+    app_name = module.params["name"] or module.params["id"]
 
-    app_label = module.params['label'] or module.params['name']
-    app_domain = module.params['domain']
-    app_settings = module.params['settings']
+    app_label = module.params["label"] or module.params["name"]
+    app_domain = module.params["domain"]
+    app_settings = module.params["settings"]
     app_args = urlencode({**dict(domain=app_domain), **app_settings})
-    app_desired_state = module.params['state']
+    app_desired_state = module.params["state"]
     # app_upgraded = module.params['upgraded']
     # app_force = module.params['force']
     # TODO: add option to handle backup and/or purge?
 
-
-#  Check if app exists
+    #  Check if app exists
 
     previous = get_app_info(app_name, True)
-    app_was_present = True if previous else False
+    app_was_present = bool(previous)
 
-    result = dict(
-        changed=False)
+    result = dict(changed=False)
 
     # Apply changes in order
-    if not app_was_present and app_desired_state == 'absent':
-        result['changed'] = False
+    if not app_was_present and app_desired_state == "absent":
+        result["changed"] = False
         module.exit_json(**result)
 
     #  Uninstall if needed
-    elif app_was_present and app_desired_state == 'absent':
+    elif app_was_present and app_desired_state == "absent":
 
-        result['uninstalled'] = True
-        result['changed'] = True
+        result["uninstalled"] = True
+        result["changed"] = True
         if module.check_mode:
             module.exit_json(**result)
 
-        command = ['/usr/bin/yunohost',
-                   'app', 'remove', app_id]
-        change = Popen(
-            command,
-            stdout=PIPE,
-            stderr=PIPE)
-        result['command'] = command
+        command = ["/usr/bin/yunohost", "app", "remove", app_id]
+        change = Popen(command, stdout=PIPE, stderr=PIPE)
+        result["command"] = command
         stdout, stderr = change.communicate()
 
         #   If app doesn't exist, create it with given params
-    elif not app_was_present and app_desired_state == 'present':
+    elif not app_was_present and app_desired_state == "present":
 
-        result['installed'] = True
-        result['changed'] = True
-        result['app_args'] = app_args
+        result["installed"] = True
+        result["changed"] = True
+        result["app_args"] = app_args
 
         if module.check_mode:
             module.exit_json(**result)
 
-        command = ['/usr/bin/yunohost',
-                   'app', 'install', app_name, '--label', app_label, '--args', app_args, '--force']
+        command = [
+            "/usr/bin/yunohost",
+            "app",
+            "install",
+            app_name,
+            "--label",
+            app_label,
+            "--args",
+            app_args,
+            "--force",
+        ]
         change = Popen(
             command,
             # domain=domain.tld&path=/path
             stdout=PIPE,
-            stderr=PIPE
+            stderr=PIPE,
         )
-        result['command'] = command
+        result["command"] = command
         stdout, stderr = change.communicate()
 
-    elif app_was_present and app_desired_state == 'present':
+    elif app_was_present and app_desired_state == "present":
         # This module doesn't support app modifications yet.
-        result['changed'] = False
-        result['app_args'] = app_args
+        result["changed"] = False
+        result["app_args"] = app_args
 
-        result['msg'] = "This module doesn't support app modifications yet."
-        result['state'] = get_app_info(app_name)
+        result["msg"] = "This module doesn't support app modifications yet."
+        result["state"] = get_app_info(app_name)
         module.exit_json(**result)
         # TODO:
         # determine changes to do
@@ -262,16 +275,16 @@ def run_module():
 
     else:
         module.fail_json(
-            msg="Logic error: make sure 'state' is either present or absent")
+            msg="Logic error: make sure 'state' is either present or absent"
+        )
 
-    result['state'] = get_app_info(app_name)
+    result["state"] = get_app_info(app_name)
 
-    result['stdout'] = stdout.decode('UTF-8')
-    result['stderr'] = stderr.decode('UTF-8')
-    result['rc'] = change.returncode
-    if(change.returncode != 0):
-        module.fail_json(
-            msg="Error when proceeding to change", **result)
+    result["stdout"] = stdout.decode("UTF-8")
+    result["stderr"] = stderr.decode("UTF-8")
+    result["rc"] = change.returncode
+    if change.returncode != 0:
+        module.fail_json(msg="Error when proceeding to change", **result)
     else:
         module.exit_json(**result)
 
@@ -280,5 +293,5 @@ def main():
     run_module()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
